cf64ff1c208d1ba544bbba3ffecdd7fafc3f65e8
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-05-27 14:22:07 -0700

    Automatic background drawing, copied from BuildingGadgets#277

--- src/main/java/powerlessri/harmonics/HarmonicsCore.java
+++ src/main/java/powerlessri/harmonics/HarmonicsCore.java
@@ -2,4 +2,8 @@ package powerlessri.harmonics;
 
 public class HarmonicsCore {
 
+    public static final String MODID = "harmonicscore";
+    public static final String NAME = "Harmonics Core";
+    public static final String VERSION = "1.0.0";;
+
 }
