62d1cd987fa8856db6640b40fb51f4e12d21ad6f
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-07-19 23:02:49 -0700

    Update to 1.14.3

--- src/main/java/powerlessri/harmonics/HarmonicsCore.java
+++ src/main/java/powerlessri/harmonics/HarmonicsCore.java
@@ -4,6 +4,6 @@ public class HarmonicsCore {
 
     public static final String MODID = "harmonicscore";
     public static final String NAME = "Harmonics Core";
-    public static final String VERSION = "1.0.0";;
+    public static final String VERSION = "1.0.0";
 
 }
