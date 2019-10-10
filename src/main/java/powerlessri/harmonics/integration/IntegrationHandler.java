b6266fb07f8615c51ceee87b30768b365b4e0bc4
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-10-09 19:23:10 -0700

    JEI integration

--- src/main/java/powerlessri/harmonics/integration/IntegrationHandler.java
+++ src/main/java/powerlessri/harmonics/integration/IntegrationHandler.java
@@ -1,5 +1,6 @@
 package powerlessri.harmonics.integration;
 
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public final class IntegrationHandler {
@@ -8,6 +9,13 @@ public final class IntegrationHandler {
     }
 
     public static void setup(FMLCommonSetupEvent event) {
-        
+    }
+
+    public @interface IntegratedMod {
+
+        /**
+         * Mod ID of the integrated mod. Loader will only load the class if the mod with given mod id is installed.
+         */
+        String value();
     }
 }
--- src/main/java/powerlessri/harmonics/integration/JEIIntegration.java
+++ src/main/java/powerlessri/harmonics/integration/JEIIntegration.java
@@ -1,15 +1,26 @@
 package powerlessri.harmonics.integration;
 
+import mcp.MethodsReturnNonnullByDefault;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
 import net.minecraft.util.ResourceLocation;
 import powerlessri.harmonics.HarmonicsCore;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
 @JeiPlugin
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 public class JEIIntegration implements IModPlugin {
 
     @Override
     public ResourceLocation getPluginUid() {
         return new ResourceLocation(HarmonicsCore.MODID, "jei_integration");
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        // TODO JEI excluded areas
     }
 }
